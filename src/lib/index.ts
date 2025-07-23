20d0563b48d5273062e62456d430649b88bb0953
Haniel Ubogu <hanieltobi@gmail.com>
2025-07-23 18:36:32 +0100

    Update package version to 0.0.3, adjust file paths in package.json, and enhance exports. Refactor index.ts to streamline inline Svelte plugin export.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,9 +1,5 @@
-import type { Component } from "svelte";
-import inlineSveltePlugin from "./plugins/inline-component.js";
-
-// Reexport your entry components here
-export { inlineSveltePlugin };
+export { default as inlineSveltePlugin } from "./plugins/inline-component.js";
 export type InlineComponent = (
   _strings: TemplateStringsArray,
   ..._vals: unknown[]
-) => Promise<Component>;
+) => Promise<import("svelte").Component>;
